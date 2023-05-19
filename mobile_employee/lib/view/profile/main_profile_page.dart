102f1b119b5b6361773b24113a6fded42d7fe5f9
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-05-19 18:59:18 +0300

    2.3 init all employee widget

---
+++ mobile_employee/lib/view/profile/main_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class MainProfilePage extends StatefulWidget {
+  @override
+  State<MainProfilePage> createState() => _MainProfilePageState();
+}
+
+class _MainProfilePageState extends State<MainProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
